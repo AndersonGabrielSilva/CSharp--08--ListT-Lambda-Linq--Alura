38e8c702c6ee1e4f7f71724a69953d01ce702963
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2020-03-17 12:52:03 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Extensoes/ListExtensoes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia.Extensoes
+{
+   public static class ListExtensoes
+    {
+        /*A criação de um metodo generico é bem semelhante com a criação de uma classe generica,
+         é somentecolocaro "<T>" indicando parao compilador que setrata deum metodo genereico.
+         apartir deste momento o metodo que antes já era uma extenção da "List" por conta da palavra reservada "this" 
+         começa tambemser generico.*/
+        public static void AdicionarVarios<T>(this List<T> listaItens,params T[] itens)
+        {
+            foreach (T item in itens)
+            {
+                listaItens.Add(item);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void teste()
+        {
+            /*Quando criamos uma lista, generica precisamosinformar o tipo dela. entre <Tipo> para poder retornar valor
+             * e tambem adicionar os itens.
+             Podemos criar listas do tipo "int"assim como do tipo "string". */
+            List<int> idades = new List<int>();
+            idades.Add(5);
+            idades.Add(15);
+            idades.Add(25);
+            idades.AdicionarVarios(7,4,5,10);
+            /*Para chamar um metodo de estensão, do tipo generico precisamos. Informar o tipo que tambem dalista. */
+            //ListExtensoes<int>.AdicionarVarios(idades,7,4,5,10);
+                        
+            List<string> nomes = new List<string>();
+            nomes.Add("Anderson Gabriel");
+            nomes.AdicionarVarios("Livia","Dulce","Jean");
+           // ListExtensoes<string>.AdicionarVarios(nomes,"Livia","Dulce","Jean");
+        
+        }
+
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using ByteBank.SistemaAgencia.Extensoes;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaListExtencoes();
+
+            Console.ReadLine();
+        }
+
+        static  void TestaListExtencoes()
+        {
+            /*O proprio dotNet já possui uma classe list, para crir uma lista.´Por serbastante ultilizada pelos desenvolvedores.
+             Ela possui alguns metodos como por exemplo:
+             ".Add" = Que adiciona itens a lista..
+             ".AddRange" = Adicionaos elementos de um array ao final de nossa lista.
+             ".Remove" = Que remove os itens da lista.
+             ".RemoveAll" = Que remove todos itens da lista.
+             ".Count" = Retorna a quantidadedos itens presentesna lista(Tamanho).
+             ".Contains" = Verifica se alguem item esta na lista.
+             ".ToArray" = Copia todos os elementos de uma listapara outra 
+             */
+            List<int> idades = new List<int>();
+
+            idades.Add(5);
+            idades.Add(10);
+            idades.Add(15);
+            idades.Add(55);
+            idades.AddRange(new int[] { 1, 2, 3, 4, 5, 10 });
+            
+            idades.AdicionarVarios(15, 20, 14, 78, 55555);
+            //ListExtensoes.AdicionarVarios(idades,45,47,10,23,94,100);
+
+            idades.Remove(10);
+            // idades.AdicionarVarios(1, 5, 78);
+            for (int i = 0; i < idades.Count; i++)
+            {
+
+                Console.WriteLine(idades[i]);
+
+            }
+        }
+              
+    }
+}
